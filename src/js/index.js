1515b3a8567b72187e82a340510c97e3b24f66c7
Vic Romero <vromerocomino@gmail.com>
2020-12-15 20:56:05 +0100

    Slider v2 finished

--- src/js/index.js
+++ src/js/index.js
@@ -18,23 +18,15 @@ const nav = document.getElementById("slide-nav");
 addEventListener("load", () => {
   const sliderFragment = document.createDocumentFragment();
   const navFragment = document.createDocumentFragment();
+  const leftControl = document.querySelector('.slide-control--left')
+  const rightControl = document.querySelector('.slide-control--right')
   imgs.forEach((img, index) => {
     // Slide
     const imgDiv = document.createElement("div");
     imgDiv.classList.add("img-div");
     imgDiv.setAttribute("data-id", `${index}`);
     imgDiv.style.backgroundImage = `url("${img}")`;
-    imgDiv.addEventListener("click", (e) => {
-      if (e.target.nextElementSibling) {
-        e.target.classList.remove('img-div--active');
-        e.target.nextElementSibling.classList.add('img-div--active');
-      } else {
-        e.target.classList.remove('img-div--active');
-        slider.firstElementChild.classList.add('img-div--active');
-      }
-    });
     sliderFragment.appendChild(imgDiv);
-
     // Nav
     const navDiv = document.createElement("div");
     navDiv.classList.add("nav-div");
@@ -45,16 +37,48 @@ addEventListener("load", () => {
       const navDivs = [...document.querySelectorAll('.nav-div')];
       const [activeImg] = imgDivs.filter((div) => div.classList.contains('img-div--active'));
       const [activeNav] = navDivs.filter((button) => button.classList.contains('nav-div--active'));
-      const [clickedImg] = imgDivs.filter((div) => div.dataset.id === e.target.dataset.id);
+      const [targetImg] = imgDivs.filter((div) => div.dataset.id === e.target.dataset.id);
 
       activeImg.classList.remove('img-div--active');
       activeNav.classList.remove('nav-div--active');
       e.target.classList.add('nav-div--active');
-      clickedImg.classList.add('img-div--active');
+      targetImg.classList.add('img-div--active');
     });
   });
   slider.appendChild(sliderFragment);
   nav.appendChild(navFragment);
   slider.firstElementChild.classList.add("img-div--active");
   nav.firstElementChild.classList.add("nav-div--active");
+
+  rightControl.addEventListener("click", () => {
+    const imgDivs = [...document.querySelectorAll('.img-div')];
+    const navDivs = [...document.querySelectorAll('.nav-div')];
+    const [activeImg] = imgDivs.filter((div) => div.classList.contains('img-div--active'));
+    const [activeNav] = navDivs.filter((button) => button.classList.contains('nav-div--active'));
+    activeImg.classList.remove('img-div--active');
+    activeNav.classList.remove('nav-div--active');
+    if (activeImg.nextElementSibling) {
+      activeImg.nextElementSibling.classList.add('img-div--active');
+      activeNav.nextElementSibling.classList.add('nav-div--active');
+    } else {
+      slider.firstElementChild.classList.add('img-div--active');
+      nav.firstElementChild.classList.add('nav-div--active');
+    }
+  });
+
+  leftControl.addEventListener("click", () => {
+    const imgDivs = [...document.querySelectorAll('.img-div')];
+    const navDivs = [...document.querySelectorAll('.nav-div')];
+    const [activeImg] = imgDivs.filter((div) => div.classList.contains('img-div--active'));
+    const [activeNav] = navDivs.filter((button) => button.classList.contains('nav-div--active'));
+    activeImg.classList.remove('img-div--active');
+    activeNav.classList.remove('nav-div--active');
+    if (activeImg.previousElementSibling) {
+      activeImg.previousElementSibling.classList.add('img-div--active');
+      activeNav.previousElementSibling.classList.add('nav-div--active');
+    } else {
+      slider.lastElementChild.classList.add('img-div--active');
+      nav.lastElementChild.classList.add('nav-div--active');
+    }
+  });
 });
